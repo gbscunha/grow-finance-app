821cb55dbc3f38980be42e492cef03fca90ae1ce
gbscunha <gabrielalternativo09@gmail.com>
2026-02-11 07:51:45 -0300

    chore: add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 import Image from "next/image";
+import { Button } from "./_components/ui/button";
 
-const name = '';
+const name = "";
 
 export default function Home() {
-  return (
-    <div className="flex w-screen items-center justify-center">
-      <h1 className="p-5 text-red-500">Hello World</h1>
-    </div>
-  );
+  return <Button>Click Me</Button>;
 }
